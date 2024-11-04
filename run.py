e4a69c69c0677681cdb3d4cc131bf02c38c68484
Maarten Ridder <maarten_ridder@hotmail.com>
2024-11-04 14:11:10 +0000

    added function to collect 6 last entries for all sales

--- run.py
+++ run.py
@@ -55,27 +55,25 @@ def validate_data(values):
 
     return True
 
-"""
-def update_sales_worksheet(data):
-    """
-    Update sales worksheet, add new row with the list data provided
-    """
-    print("Updating sales worksheet...\n")
-    sales_worksheet = SHEET.worksheet("sales")
-    sales_worksheet.append_row(data)
-    print("Sales worksheet updated successfully.\n")
 
+#def update_sales_worksheet(data):
+#    """
+#    Update sales worksheet, add new row with the list data provided
+#    """
+#    print("Updating sales worksheet...\n")
+#    sales_worksheet = SHEET.worksheet("sales")
+#    sales_worksheet.append_row(data)
+#    print("Sales worksheet updated successfully.\n")
+#
+#
+#def update_surplus_worksheet(data):
+#    """
+#    Update surplus worksheet, add new row with the surplus_data provided
+#    """
+#    surplus_worksheet = SHEET.worksheet("surplus")
+#    surplus_worksheet.append_row(data)
+#    print("Surplus worksheet updated successfully.\n")
 
-def update_surplus_worksheet(data):
-    """
-    Update surplus worksheet, add new row with the surplus_data provided
-    """
-
-
-    surplus_worksheet = SHEET.worksheet("surplus")
-    surplus_worksheet.append_row(data)
-    print("Surplus worksheet updated successfully.\n")
-"""
 
 def update_worksheet(data, worksheet):
     """
@@ -109,6 +107,21 @@ def calculate_surplus_data(sales_row):
     print("surplus_data")
 
 
+def get_last_5_entries_sales():
+    """
+    Collects collumns of data from sales workseet, collecting
+    the last 5 entries for each sandwich and returns the data
+    as a list of lists,
+    """
+    sales = SHEET.worksheet("sales")
+
+    columns = []
+    for ind in range(1,7):
+        column = sales.col_values(ind)
+        columns.append(column[-5:])
+    
+    return columns
+
 def main():
     """
     Run all program functions.
@@ -120,4 +133,6 @@ def main():
     update_worksheet(new_surplus_data, "surplus")
 
 print("Welcome to Love Sandwiches Data Automation")
-main()
+# main()
+
+sales_columns = get_last_5_entries_sales()
